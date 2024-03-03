01d668d852a895e90cba75ac1953fe1734f0824d
Shailyarajsinh Mahida <mahidashailyaraj@gmail.com>
2024-03-03 15:13:17 +0530

    Add react-router-dom dependency
    with Header and Footer

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+export default function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen);
+    };
+
+    return (
+        <header className="shadow sticky z-50 top-0 text-sm bg-blue-500">
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <Link to="/" className="flex items-center">
+                        <img
+                            src="https://bitinfotech.in/assets/uploads/media-uploader/bitinfotech-favicon-20201601120139.png"
+
+                            className="mr-3 h-10 w-10"
+                            alt="Logo"
+                        />
+                    </Link>
+                    <div className="flex items-center lg:order-2">
+                        <Link
+                            to="#"
+                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            to="#"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Get started
+                        </Link>
+                    </div>
+                    <div className="lg:hidden">
+                        <button
+                            onClick={toggleMobileMenu}
+                            className="text-gray-700 focus:outline-none transition-all duration-300 ease-in-out"
+                        >
+                            <svg
+                                className="h-6 w-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16m-7 6h7"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                    <div
+                        className={`${mobileMenuOpen ? 'block' : 'hidden'
+                            } lg:hidden w-full mt-4 transition-all duration-300 ease-in-out`}
+                    >
+                        <ul className=" text-sm ml-6 mr-6 flex flex-col font-medium space-y-2">
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    onClick={toggleMobileMenu}
+                                    className="  font-semibold text-black hover:text-blue-700"
+                                >
+                                    Home
+                                </NavLink>
+                                
+                            </li>
+
+                            <li>
+                                <NavLink
+                                    to="/student-corner"
+                                    onClick={toggleMobileMenu}
+                                    className=" font-semibold text-black hover:text-blue-700"
+                                >
+                                    Student Corner
+                                </NavLink>
+                                
+                            </li>
+
+                            <li>
+                                <NavLink
+                                    to="/blogs"
+                                    onClick={toggleMobileMenu}
+                                    className="  font-semibold text-black hover:text-blue-700"
+                                >
+                                   Blogs
+                                </NavLink>
+                                
+                            </li>
+                        </ul>
+                    </div>
+                    <div
+                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        id="mobile-menu-2"
+                    >
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive }) =>
+                                        ` text-base block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-blue-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-700 lg:p-0`}>
+                                    Home
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink
+                                    to="/student-corner"
+                                    className={({ isActive }) =>
+                                        ` text-base block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-blue-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-700 lg:p-0`}>
+                                    Student Corner
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
